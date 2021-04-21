a292a0952f647746f73cde8450a5826e52cadc06
Coder-Lowic <Zxf860979438>
2021-04-21 20:14:13 +0800

    week-07

---
+++ projects/stage-0/user-platform/spring-boot-oauth2/src/main/java/com/lowic/oauth/SpringBootOauth2Application.java
@@ -0,0 +1,44 @@
+package com.lowic.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * github oauth2 @see <a href="https://spring.io/guides/tutorials/spring-boot-oauth2/">spring-boot-oauth2</a>
+ * @author Lowic
+ */
+@SpringBootApplication
+@RestController
+public class SpringBootOauth2Application extends WebSecurityConfigurerAdapter {
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("login"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(a -> a.antMatchers("/", "/error", "/webjars/**").permitAll().anyRequest().authenticated())
+                .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .logout(l -> l.logoutSuccessUrl("/").permitAll())
+                .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .oauth2Login();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauth2Application.class, args);
+    }
+
+}
---
+++ projects/stage-0/user-platform/spring-boot-oauth2/src/test/java/com/lowic/oauth/SpringBootAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lowic.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
